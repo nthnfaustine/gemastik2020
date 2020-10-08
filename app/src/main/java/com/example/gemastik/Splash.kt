ad969723e6b264ec1439ebfe130342d03da28a1a
nathan <faustinenathaniel@gmail.com>
2020-10-08 07:57:12 +0700

    splashscreen login ui

---
+++ app/src/main/java/com/example/gemastik/Splash.kt
@@ -0,0 +1,22 @@
+package com.example.gemastik
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+class Splash: Activity() {
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@Splash, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
